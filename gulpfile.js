2779c2e5d1f5fb937f7cb749784163178eddefbc
Daniel Chen <daniel.xia.chen@gmail.com>
2015-12-28 19:58:40 -0800

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+// load the plugins
+var gulp      = require('gulp');
+var jshint    = require('gulp-jshint');
+var concat     = require('gulp-concat');
+var uglify     = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+var nodemon    = require('gulp-nodemon');
+
+// task for linting js files
+gulp.task('lint', function() {
+  return gulp.src(['server.js', 'public/app/*.js', 'public/app/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+// task to concatenate and minify frontend angular files
+gulp.task('angular', function() {
+  return gulp.src(['public/app/*.js', 'public/app/**/*.js'])
+    .pipe(ngAnnotate())
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/dist'));
+});
+
+// watch files to run tasks on changes 
+gulp.task('watch', function() {
+  gulp.watch(['server.js', 'public/app/*.js', 'public/app/**/*.js'], ['lint', 'angular']);
+});
+
+// start the server 
+gulp.task('nodemon', function() {
+  nodemon({
+    script: 'server.js',
+    ext: 'js css html'
+  })
+    .on('start', ['watch'])
+    .on('change', ['watch'])
+    .on('restart', function() {
+      console.log('Restarted!');
+    });
+});
+
+gulp.task('default', ['nodemon']);
