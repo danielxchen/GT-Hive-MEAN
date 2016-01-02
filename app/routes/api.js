2ef99c6bf27d120923eda092f9a5f82a23b38b34
Daniel Chen <daniel.xia.chen@gmail.com>
2016-01-02 12:03:46 -0800

    Update api endpoints

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,19 +1,36 @@
 // Database models
 var Building = require('../models/building');
+var Floor = require('../models/floor');
 
 module.exports = function(app, express) {
 
 	var apiRouter = express.Router();
 
-	// Get all buildings 
-	app.get('/buildings', function(req, res) {
-		Building.find(function(err, buildings) {
-			if (err) res.send(err);
+	// Routes that end in /buildings
+	apiRouter.route('/buildings')
 
-			// return the buildings
-			res.json(buildings);
+		// Get all buildings 
+		.get(function(req, res) {
+			Building.find(function(err, buildings) {
+				if (err) res.send(err);
+
+				// return the buildings
+				res.json(buildings);
+			});
 		});
-	});
+
+	// Routes that end in /buildings/:bid
+	apiRouter.route('buildings/:bid')
+
+		// Get the floors of the building with that bid
+		.get(function(req, res) {
+			Floor.find({ bid: req.params.bid }, function(err, floors) {
+				if (err) res.send(err);
+
+				// return floors
+				res.json(floors);
+			});
+		});
 
 	return apiRouter;
 };
