d5e15f6d44cf94f74fb34e7f832103ff2a18081a
Daniel Chen <daniel.xia.chen@gmail.com>
2016-01-02 15:06:53 -0800

    Update building service

--- public/app/services/buildingService.js
+++ public/app/services/buildingService.js
@@ -4,26 +4,14 @@ angular.module('buildingService', [])
 
 	var buildingFactory = {};
 
+	// Get all buildings
 	buildingFactory.all = function() {
-		return $http.get('/api/angular/buildings');
+		return $http.get('/api/buildings');
 	};
 
-	buildingFactory.getOccupancy = function(bid) {
-		return $http.get('/api/locationinfo/b_id=' + bid);
-	};
-
-	buildingFactory.updateOccupancies = function(bids) {
-
-		var str = '';
-		var length = bids.length;
-		for (var i = 0; i < length - 1; i++) {
-			str += 'b_id=' + bids[i] + '&';
-		}
-		str += 'b_id=' + bids[length - 1];
-
-		//$log.log(str);
-
-		return $http.get('/api/locationinfo/' + str);
+	// Get a single building
+	buildingFactory.get = function(bid) {
+		return $http.get('/api/buildings/' + bid);
 	};
 
 	return buildingFactory;
