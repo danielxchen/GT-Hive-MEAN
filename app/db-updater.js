8ccceede22f5f172d46bb58c1bd6d256e95ca019
Daniel Chen <daniel.xia.chen@gmail.com>
2016-01-04 18:23:55 -0800

    Get task to update one building succesfully

--- app/db-updater.js
+++ app/db-updater.js
@@ -1,12 +1,31 @@
+// Get modules 
 var CronJob = require('cron').CronJob;
-var Promise = require("bluebird");
-var Building = require('../models/building');
-Promise.promisifyAll(require("request"));
+var Promise = require('bluebird');
+var request = Promise.promisifyAll(require('request'));
+var Building = require('./models/building');
 Promise.promisifyAll(Building);
 Promise.promisifyAll(Building.prototype);
 
-module.exports = new CronJob('* * * * * *', function() {
+module.exports = new CronJob('* */5 * * * *', function() {
+	console.log('Job Starting');
 
-  console.log('You will see this message every second');
+	// Get the current occupancy of the building 
+  	var url = 'http://gtwhereami.herokuapp.com/locationinfo?bid=166';
+  	request.getAsync(url).then(function(res) {
+  		// Parse the response into JSON
+  		return JSON.parse(res.body);
 
-}, null, false, 'America/New_York');
+  	}).then(function(parsed) {
+  		// Update the building's occupancy
+  		return Building.findOneAndUpdateAsync({ bid: 166 }, { $set: { occupancy:parsed.occupancy } }, { new: true });
+
+  	}).then(function(doc) {
+  		// We got the updated document
+  		console.log(doc);
+
+  	}).catch(function(err) {
+  		// Catch any error that happened along the way
+  		console.log('Error occured:', err);
+  	});
+
+}, null, true, 'America/New_York');
