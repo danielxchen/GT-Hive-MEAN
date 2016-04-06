9bb1edb334a921e21b8bf5944f3a9cf928dd0ff4
Justin Buchanan <justbuchanan@gmail.com>
2016-04-06 19:05:55 -0400

    working on updating building graph with actual data

--- public/app/controllers/buildingCtrl.js
+++ public/app/controllers/buildingCtrl.js
@@ -1,30 +1,15 @@
 angular.module('buildingCtrl', ['buildingService', 'chart.js'])
 
-.controller('buildingController', function($scope) {
-    // TODO(justin): setup chart, data
-        // <script type="text/javascript">
-        //     var data = {
-        //         labels: ['monday', 'tuesday', 'wednesday', 'thursday'],
-        //         datasetes: [
-        //             {
-        //                 label: "set 1",
-        //                 fillColor: "rgba(220,220,220,0.2)",
-        //                 strokeColor: "rgba(220,220,220,1)",
-        //                 pointColor: "rgba(220,220,220,1)",
-        //                 pointStrokeColor: "#fff",
-        //                 pointHighlightFill: "#fff",
-        //                 pointHighlightStroke: "rgba(220,220,220,1)",
-        //                 data: [65, 59, 80, 81, 56, 55, 40]
-        //             }
-        //         ]
-        //     }
-        //     var ctx = document.getElementById("occupancyVsTime").getContext("2d");
-        //     var chart = new Chart(ctx).Line(data, options);
-        // </script>
-
+.controller('buildingController', function($scope, $routeParams, Building) {
     $scope.labels = ['monday', 'tuesday', 'wednesday', 'thursday'];
     $scope.series = ['Series A'];
     $scope.data = [
         [65, 59, 80, 81]
     ];
+
+    console.log("Building page for building_id=" + $routeParams.bid);
+
+    Building.get($routeParams.bid).then( function(res) {
+        console.log(res);
+    });
 });
