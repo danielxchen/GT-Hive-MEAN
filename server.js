13ec55de7a5bc22b511768c8952d361f31fe02d6
Justin Buchanan <justbuchanan@gmail.com>
2016-04-06 14:14:02 -0400

    initial implementation of GT CAS

--- server.js
+++ server.js
@@ -3,10 +3,12 @@
 
 // Get packages
 var express    = require('express');		
+var session    = require('express-session');
 var app        = express(); 				// define our app using express
 var mongoose   = require('mongoose');
 var config 	   = require('./config');
 var path 	   = require('path');
+var CASAuth        = require('cas-authentication');
 
 // APP CONFIGURATION ==================
 // ====================================
@@ -31,6 +33,23 @@ app.use(function(req, res, next) {
 var dbUpdater = require('./app/db-updater');
 dbUpdater.start();
 
+// Setup CAS
+// See this url for more info: https://www.npmjs.com/package/cas-authentication
+// ====================================
+app.use(session({
+    secret: 'super secret key', // TODO(justin): fix
+    resave: false,
+    saveUninitialized: true
+}));
+
+var cas = new CASAuth({
+    cas_url: 'https://login.gatech.edu/cas',
+    service_url: 'http://gthive.me', // TODO(justin): pull dynamically so we can have testing subdomains?
+    session_name: 'cas_user',
+    session_info: 'cas_userinfo',
+    destroy_session: false
+});
+
 // ROUTES FOR OUR API =================
 // ====================================
 
@@ -39,9 +58,19 @@ app.use('/api', apiRoutes);
 
 // MAIN CATCHALL ROUTE --------------- 
 // SEND USERS TO FRONTEND ------------
-app.get('*', function(req, res) {
+// Unauthenticated clients will be redirected to the CAS login and then back to 
+// this route once authenticated. 
+app.get('/', cas.bounce, function(req, res) {
 	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
 });
+
+// Unauthenticated clients will be redirected to the CAS login and then to the 
+// provided "redirectTo" query parameter once authenticated. 
+app.get('/authenticate', cas.bounce_redirect);
+
+// This route will de-authenticate the client with the Express server and then 
+// redirect the client to the CAS logout page. 
+app.get('/logout', cas.logout);
 
 // START THE SERVER ===================
 // ====================================
