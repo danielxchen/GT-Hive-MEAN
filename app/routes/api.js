9b8d991612d1876f0dce0c67351f2d56dd0f9d49
Justin Buchanan <justbuchanan@gmail.com>
2016-04-24 13:30:11 -0400

    setup api proxy

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,6 +1,8 @@
 // Database models
 var Building = require('../models/building');
 var Floor = require('../models/floor');
+var Promise = require('bluebird');
+var request = Promise.promisifyAll(require('request'));
 
 module.exports = function(app, express) {
 
@@ -24,6 +26,15 @@ module.exports = function(app, express) {
 
 			// return floors
 			res.json(floors);
+		});
+	});
+
+	// proxy api requests to rnoc via vpn so the app can get results without being on the gatech network
+	apiRouter.get('/proxy', function(req, res) {
+		var url = 'http://wifi.dssg.rnoc.gatech.edu:3000/api/count?details=true';
+		console.log("making proxy request to rnoc api");
+		request.getAsync(url).then(function(apiRes) {
+			res.body = apiRes.body;
 		});
 	});
 
