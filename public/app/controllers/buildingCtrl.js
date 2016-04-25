895a8e374fc5ed06e097413daa54b1e638b88b0e
Justin Buchanan <justbuchanan@gmail.com>
2016-04-25 02:41:51 -0400

    legends for graphs

--- public/app/controllers/buildingCtrl.js
+++ public/app/controllers/buildingCtrl.js
@@ -3,12 +3,12 @@ angular.module('buildingCtrl', ['buildingService', 'chart.js'])
 .controller('buildingController', function($scope, $routeParams, Building) {
     $scope.labels = ['12am', '1am', '2am', '3am', '4am', '5am', '6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm'];
     $scope.series = ['Average', 'Today'];
+    $scope.legend = true;
 
     $scope.data = [];
 
     Building.graphdata($routeParams.bid).then(function(res) {
         console.log("got graph data for bid=", $routeParams.bid);
         $scope.data = [res.data.averages, res.data.today];
-        console.log(res.data);
     });
 });
